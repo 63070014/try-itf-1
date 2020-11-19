33570426416fd3ee0483c7c8bb12a7e18a0ab490
62100006 <74727225+62100006@users.noreply.github.com>
2020-11-20 02:09:00 +0700

    Update show.php

--- show.php
+++ show.php
@@ -11,7 +11,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'datadata.mysql.database.azure.com', 'datadata@datadata', 'PuiFai2023', 'itf-lab', 3306);
+mysqli_real_connect($conn, 'datadata.mysql.database.azure.com', 'datadata@datadata', 'PuiFai2023', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
